b3ab3b038422cf2afdb0ef7ee653158c6c8bb31a
Brandon D. <alyssajoseph415@gmail.com>
2025-04-05 14:35:32 -0700

    Message

---
+++ src/components/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function aboutUs(){
+    return(
+        <div class="welcome">
+            <h1>Ease of Health</h1>
+            <h2>A quicker and reliable health facility finder</h2>
+
+            <h1>About:</h1>
+            <p>Ease of Health is a website to help users find nearby hospitals quickly. With accessible and user-friendly search categories, users are able to find the health facilities they need.</p>
+        </div>
+    );
+}
+
+export default aboutUs
