04c74109c981eb61160377ee29038dc9fdd37de7
Brandon D. <alyssajoseph415@gmail.com>
2025-04-06 17:48:52 -0700

    yes

--- src/components/finder/map.js
+++ src/components/finder/map.js
@@ -25,7 +25,7 @@ export default function HospitalMap(){
     return(
         <APIProvider apiKey={env.REACT_APP_API}>
             <div style={{height: "100vh", width: "100%"}}>
-                <Map zoomControl={true} streetViewControl={true}
+                <Map zoomControl={true} streetViewControl={true} disableDefaultUI={true}
                  mapTypeControl={false} scrollwheel={true}
                  disableDoubleClickZoom={false}
                  zoom={7} center={position} mapId={"e82a49a0aed5df04"}>
